9ab971c04437d6d33c1a4fa35dbfd6fb9e334f45
Radwanul Islam Nayeem <103718355+paradox-99@users.noreply.github.com>
2024-04-28 06:25:24 +0600

    added details page

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/headers/Navbar';
+import Footer from './components/footer/Footer';
 
 const Root = () => {
     return (
         <div className='bg-[#D8FDE3]'>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
